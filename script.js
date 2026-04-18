35a71299139898b4aea864985dc4e8a9cb7933f7
y02913214-bit <y02913214@gmail.com>
2026-04-18 15:09:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-setTimeout(() => {
-    if (localStorage.getItem("isLoggedIn") === "true") {
-        window.location.href = "home.html";
-    } else {
-        window.location.href = "login.html";
-    }
-}, 3000);
+if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
+
+    setTimeout(() => {
+        if (localStorage.getItem("isLoggedIn") === "true") {
+            window.location.href = "home.html";
+        } else {
+            window.location.href = "login.html";
+        }
+    }, 3000);
+
+}
 /* ===== تنقل ===== */
 function goRegister() {
   window.location.href = "register.html";
